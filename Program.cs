feb47453a8477dff3cab72c9c3cfaeb58e773b77
Nickolas Giffen <z1910335@students.niu.edu>
2023-10-08 17:35:39 -0500

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,42 +1,61 @@
-﻿
+﻿using System;
 using System.Diagnostics;
 using System.Globalization;
-//ch-ch-ch-chia
+
 class Program
 {
     static void Main()
     {
-        int n = 80000; // number of random numbers to generate
+        int n = 800000; // number of random numbers to generate
         double[,] numbers;
         Stopwatch timer = new Stopwatch();
 
         numbers = GenerateRandomNumbers(n);
 
-        //First Timer
         timer.Start();
-        AddNumbers(numbers,n);
+        DirectMult(numbers,n);
         timer.Stop();
-        Console.WriteLine("Additions");
+        Console.WriteLine("Direct Multiplication");
         Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + " ms   "
                             + timer.ElapsedTicks + " ticks\n");
-        float addTicks = timer.ElapsedTicks;
+        float directTicks = (float)timer.ElapsedTicks;
 
-        //Second Timer
-        timer.Start();
-        MultiplyNumbers(numbers,n);
-        timer.Stop();
-        Console.WriteLine("Multiplications");
-        Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + " ms   "
-                            + timer.ElapsedTicks + " ticks\n");
-        float multTicks = timer.ElapsedTicks;
+        timer.Reset();  //RESET THE TIMER (ADD THIS FOR EVERY ADDITIONAL OPERATION)
 
-        Console.WriteLine("Ratio = " + addTicks/multTicks);
-        Console.WriteLine("Ratio = " + multTicks/addTicks);
-
-        // int i;
+        // int i; // This is to test that values generated as expected
         // for(i=0; i<n; ++i)
         // {
-        // Console.WriteLine(numbers[i,0] + " " + numbers[i,1] + " " + numbers[i,2]);
+        // Console.WriteLine(numbers[i,0] + " " + numbers[i,1]);
+        // }
+
+        timer.Start();
+        MathPower(numbers,n);
+        timer.Stop();
+        Console.WriteLine("Power Function");
+        Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + " ms   "
+                            + timer.ElapsedTicks + " ticks\n");
+        float powerTicks = (float)timer.ElapsedTicks;
+
+        // int i; // This is to test that values generated as expected
+        // for(i=0; i<n; ++i)
+        // {
+        // Console.WriteLine(numbers[i,0] + " " + numbers[i,1]);
+        // }
+
+        timer.Reset();  //RESET THE TIMER (ADD THIS FOR EVERY ADDITIONAL OPERATION)
+
+       timer.Start();
+        MathSquareroot(numbers,n);
+        timer.Stop();
+        Console.WriteLine("Square Root Function");
+        Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + " ms   "
+                            + timer.ElapsedTicks + " ticks\n");
+        float sqrtTicks = (float)timer.ElapsedTicks;
+
+        // int i; // This is to test that values generated as expected
+        // for(i=0; i<n; ++i)
+        // {
+        // Console.WriteLine(numbers[i,0] + " " + numbers[i,1]);
         // }
     }
 
@@ -47,16 +66,15 @@ class Program
         double[,] num = new double[count,3]; // make array
         for(int i=0; i<count; ++i)
         {
-            num[i,0] = 100.0*rand.NextDouble(); //fill in elements of array
-            num[i,1] = 100.0*rand.NextDouble();
+            num[i,0] = 10000.0*rand.NextDouble(); //fill in elements of array
         }
 
-        // double number = 10.0*rand.NextDouble();
         return num; // returns reference array
     }
 
-    // Function that adds numbers in the supplied 2d array
-    static void AddNumbers(double[,] nums, int count)
+
+    // Function that uses direct multiplication
+    static void DirectMult(double[,] nums, int count)
     {
         int i;
         for(i=0; i<count; ++i)
@@ -64,14 +82,22 @@ class Program
             nums[i,1] = nums[i,0] * nums[i,0];
         }
     }
-
-    // Function that multiplies numbers in the supplied 2d array
-    static void MultiplyNumbers(double[,] nums, int count)
+    // Function that uses Math.Pow
+    static void MathPower(double[,] nums, int count)
     {
         int i;
         for(i=0; i<count; ++i)
         {
-            nums[i,2] = nums[i,0] * nums[i,1];
+            nums[i,1] = Math.Pow(nums[i,0],2);
+        }
+    }
+    // Function that uses Math.Sqrt Function
+        static void MathSquareroot(double[,] nums, int count)
+    {
+        int i;
+        for(i=0; i<count; ++i)
+        {
+            nums[i,1] = Math.Sqrt(nums[i,0]);
         }
     }
 }
