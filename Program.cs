918c3924967569b5a1eea9b5f987dc25713510b9
Nickolas Giffen <z1910335@students.niu.edu>
2023-10-08 18:00:37 -0500

    Group A Question 1

--- Program.cs
+++ Program.cs
@@ -6,57 +6,44 @@ class Program
 {
     static void Main()
     {
-        int n = 800000; // number of random numbers to generate
+        int n = 80000; // number of random numbers to generate
         double[,] numbers;
         Stopwatch timer = new Stopwatch();
 
         numbers = GenerateRandomNumbers(n);
 
         timer.Start();
-        DirectMult(numbers,n);
+        AddDoubles(numbers,n);
         timer.Stop();
-        Console.WriteLine("Direct Multiplication");
+        Console.WriteLine("Doubles");
         Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + " ms   "
                             + timer.ElapsedTicks + " ticks\n");
-        float directTicks = (float)timer.ElapsedTicks;
+        float doubleTicks = (float)timer.ElapsedTicks;
 
         timer.Reset();  //RESET THE TIMER (ADD THIS FOR EVERY ADDITIONAL OPERATION)
 
-        // int i; // This is to test that values generated as expected
+        // int i;
         // for(i=0; i<n; ++i)
         // {
-        // Console.WriteLine(numbers[i,0] + " " + numbers[i,1]);
+        // Console.WriteLine(numbers[i,0] + " " + numbers[i,1] + " " + numbers[i,2]);
         // }
 
         timer.Start();
-        MathPower(numbers,n);
+        AddFloats(numbers,n);
         timer.Stop();
-        Console.WriteLine("Power Function");
+        Console.WriteLine("Floats");
         Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + " ms   "
                             + timer.ElapsedTicks + " ticks\n");
-        float powerTicks = (float)timer.ElapsedTicks;
+        float floatTicks = (float)timer.ElapsedTicks;
 
-        // int i; // This is to test that values generated as expected
+        Console.WriteLine("Ratio = " + doubleTicks/floatTicks);
+
+        // int i;
         // for(i=0; i<n; ++i)
         // {
-        // Console.WriteLine(numbers[i,0] + " " + numbers[i,1]);
+        // Console.WriteLine(numbers[i,0] + " " + numbers[i,1] + " " + numbers[i,2]);
         // }
 
-        timer.Reset();  //RESET THE TIMER (ADD THIS FOR EVERY ADDITIONAL OPERATION)
-
-       timer.Start();
-        MathSquareroot(numbers,n);
-        timer.Stop();
-        Console.WriteLine("Square Root Function");
-        Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + " ms   "
-                            + timer.ElapsedTicks + " ticks\n");
-        float sqrtTicks = (float)timer.ElapsedTicks;
-
-        // int i; // This is to test that values generated as expected
-        // for(i=0; i<n; ++i)
-        // {
-        // Console.WriteLine(numbers[i,0] + " " + numbers[i,1]);
-        // }
     }
 
     // Function to generate an array of random numbers
@@ -67,37 +54,30 @@ class Program
         for(int i=0; i<count; ++i)
         {
             num[i,0] = 10000.0*rand.NextDouble(); //fill in elements of array
+            num[i,1] = 10000.0*rand.NextDouble();
         }
 
+        // double number = 10.0*rand.NextDouble();
         return num; // returns reference array
     }
 
 
-    // Function that uses direct multiplication
-    static void DirectMult(double[,] nums, int count)
+    // Function that adds Doubles in the supplied 2d array
+    static void AddDoubles(double[,] nums, int count)
     {
         int i;
         for(i=0; i<count; ++i)
         {
-            nums[i,1] = nums[i,0] * nums[i,0];
+            nums[i,2] = nums[i,0] + nums[i,1];
         }
     }
-    // Function that uses Math.Pow
-    static void MathPower(double[,] nums, int count)
+    // Function that adds Floats in the supplied 2d array
+    static void AddFloats(double[,] nums, int count)
     {
         int i;
         for(i=0; i<count; ++i)
         {
-            nums[i,1] = Math.Pow(nums[i,0],2);
-        }
-    }
-    // Function that uses Math.Sqrt Function
-        static void MathSquareroot(double[,] nums, int count)
-    {
-        int i;
-        for(i=0; i<count; ++i)
-        {
-            nums[i,1] = Math.Sqrt(nums[i,0]);
+            nums[i,2] = (float)nums[i,0] + (float)nums[i,1];
         }
     }
 }
